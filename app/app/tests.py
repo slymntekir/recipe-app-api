45908d8e230d5b3463a7a0cc3c28dcea186cb052
Süleyman Tekir <stekir@bddk.org.tr>
2023-09-03 01:00:59 +0300

    Last 03.09.2023.

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,7 +5,7 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calc
 
-class CalcTests(SimpleTestCase):
+class CalcTests(SimpleTestCase): 
     """Test the calc module."""
 
     def test_add_numbers(self):
@@ -14,9 +14,9 @@ class CalcTests(SimpleTestCase):
 
         self.assertEqual(res, 11)
 
+
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
-
